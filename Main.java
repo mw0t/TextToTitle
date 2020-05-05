09a0f8f0c35b7beac6b426b6d0d7fd789659314b
khaled <khaled@live.cl>
2020-05-05 01:40:24 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.lang.Thread;
+
+class Main {
+
+  public static final String GREEN = "\u001B[32m";
+  public static final String RESET = "\u001B[0m";
+
+  public static void main(String[] args) {
+
+    Scanner Text = new Scanner(System.in);
+    System.out.print(GREEN+"\n\nEnter Your Text > "+RESET);
+
+    String org= Text.nextLine();
+      String [] temp=org.split(" ");
+      int len=temp.length;
+      String ne = ".";
+       long start = System.currentTimeMillis();
+       System.out.print("Your Title Is: ");
+      for(int i=0;i<len;i++){
+         temp[i]=temp[i].toUpperCase();
+         temp[i]=(temp[i].substring(0, 1)).toUpperCase()+(temp[i].substring(1, temp[i].length())).toLowerCase();
+         System.out.print(GREEN+temp[i]+" ");
+      }
+          long finish = System.currentTimeMillis();
+          long timeElapsed = finish - start;
+         System.out.print(RESET+"\n"+timeElapsed+" MilliSeconds");
+         Runtime.getRuntime().addShutdownHook(new ShutdownHook());
+
+        }
+}
---
+++ ShutdownHook.java
@@ -0,0 +1,7 @@
+class ShutdownHook extends Thread
+{
+    public void run()
+    {
+      System.out.println("\n\nThank you!\n Don't forget to follow me on Instagram: "+Main.GREEN+"@mw0t");
+        }
+}
